090fc5e22fe53877b44c3c414150774e3aef9261
Prateek Sachan <psachan@cs.iitr.ac.in>
2021-03-24 11:55:54 +0530

    Add http server and publish body to nats

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# http-nats-adapter
+# HTTP NATS Adapter
+
+## Installation
+
+```bash
+$ luarocks install --local https://raw.githubusercontent.com/DawnAngel/lua-nats/master/rockspec/nats-0.0.3-1.rockspec
+$ luarocks install --local http
+
+```
+
+## Run the adapter
+
+```bash
+$ lua adapter.lua
+```
---
+++ adapter.lua
@@ -0,0 +1,70 @@
+package.path = './deps/?.lua;' .. package.path
+pcall(require, 'luarocks.require')
+
+local nats = require 'nats'
+local http_server = require "http.server"
+local http_headers = require "http.headers"
+
+local http_port = 8080
+local params = {
+    host = '127.0.0.1',
+    port = 4222,
+}
+
+local client = nats.connect(params)
+
+-- client:enable_trace()
+-- client:set_auth('user', 'password')
+client:connect()
+
+local function reply(myserver, stream) -- luacheck: ignore 212
+	-- Read in headers
+	local req_headers = assert(stream:get_headers())
+	local req_method = req_headers:get ":method"
+
+	-- Log request to stdout
+	assert(io.stdout:write(string.format('[%s] "%s %s HTTP/%g"  "%s" "%s"\n',
+		os.date("%d/%b/%Y:%H:%M:%S %z"),
+		req_method or "",
+		req_headers:get(":path") or "",
+		stream.connection.version,
+		req_headers:get("referer") or "-",
+		req_headers:get("user-agent") or "-"
+	)))
+
+	-- Build response headers
+	local res_headers = http_headers.new()
+	res_headers:append(":status", "200")
+	res_headers:append("content-type", "text/plain")
+	-- Send headers to client; end the stream immediately if this was a HEAD request
+	assert(stream:write_headers(res_headers, req_method == "HEAD"))
+	if req_method ~= "HEAD" then
+		-- Send body, ending the stream
+		assert(stream:write_chunk("Hello world!\n", true))
+    end
+    
+    local body = stream:get_body_as_string(1)
+    client:publish('Mesaage', body)
+end
+
+local listen = assert(http_server.listen {
+	host = "localhost";
+	port = http_port;
+	onstream = reply;
+	onerror = function(myserver, context, op, err, errno)
+		local msg = op .. " on " .. tostring(context) .. " failed"
+		if err then
+			msg = msg .. ": " .. tostring(err)
+		end
+		assert(io.stderr:write(msg, "\n"))
+	end;
+})
+
+assert(listen:listen())
+do
+	local bound_port = select(3, listen:localname())
+	assert(io.stderr:write(string.format("Now listening on port %d\n", bound_port)))
+end
+
+-- Start the main server loop
+assert(listen:loop())
